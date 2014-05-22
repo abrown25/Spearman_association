03265ce72c4b901651a1c15a85ff3f31a76b5607
abrown25 <aabrown24@hotmail.com>
2014-05-22 16:01:01 +0100

    Branch to allow missing data

---
+++ missing/arg_parse.d
@@ -0,0 +1,176 @@
+import std.array : split;
+import std.c.stdlib : exit;
+import std.conv : to, ConvException;
+import std.stdio : writeln;
+import std.string : chompPrefix, startsWith;
+
+class Opts{
+  bool run = false;
+  bool give_seed = false;
+  bool min = false;
+  bool pval = false;
+  bool pid = false;
+  bool gid = false;
+  bool nocheck = false;
+  int number = 0;
+  int seed = 0;
+  int skip = 0;
+  int phenC = 0;
+
+  this(string[string] option){
+    getGenotypeSkip(option);
+    getPhenColumn(option);
+    getFlags(option);
+    getPerms(option);
+  }
+
+  private void getGenotypeSkip(string[string] option){
+    auto p = "gs" in option;
+    try{
+      skip = p ? to!int(*p) : 0;
+    } catch (ConvException e){
+      writeln("Failed to run analysis: Non-numeric argument to -geno-skip");
+      exit(0);
+    }
+  }
+
+  private void getPhenColumn(string[string] option){
+    auto p = "pc" in option;
+  try{
+    phenC = p ? to!int(*p) - 1
+      : "pid" in option ? 1 : 0;
+  } catch (ConvException e){
+    writeln("Failed to run analysis: Non-numeric argument to -pheno-col");
+    exit(0);
+  }
+}
+
+  private void getFlags(string[string] option){
+    if ("nocheck" in option)
+      nocheck = true;
+    if ("pid" in option)
+      pid = true;
+    if ("gid" in option)
+      gid = true;
+    if ("fwer" in option)
+      min = true;
+    if ("pval" in option)
+      pval = true;
+
+    if (min && pval)
+      {
+	writeln("Failed to run analysis: Both -fwer and -pval flag specified");
+	exit(0);
+      }
+
+    if ((min || pval) && !("perm" in option))
+      {
+	writeln("Failed to run analysis: Permutations must be specified with the -perm flag");
+	exit(0);
+      }
+  }
+
+  private void getPerms(string[string] option){
+    auto p = "perm" in option;
+    if (p)
+      {
+	run = true;
+	string[] value = split(*p, ",");
+	try{
+	  number = to!int(value[0]);
+	} catch (ConvException e){
+	  writeln("Failed to run analysis: Non-integer argument to -perm");
+	  exit(0);
+	}
+	if (value.length==2)
+	  {
+	    give_seed = true;
+	    try{
+	      seed = to!int(value[1]);
+	    } catch (ConvException e){
+	      writeln("Failed to run analysis: Non-integer argument to seed");
+	      exit(0);
+	    }
+	  }
+      }
+  }
+}
+
+static immutable auto helpString = "Usage: spearman [options]:
+Options:
+    --help           : display help file
+    -pheno, -p       : phenotype file [default: last argument]
+    -geno, -g        : genotype file [default stdin]
+    -output, -o      : output file [default stdout]
+    -cov             : optional covariates file, if specified analysis will be performed on the residuals, after controlling for covariates with least squares regression
+    -pheno-id, -pid  : phenotype IDs are in the first column, if genotype IDs are also present then we check for mismatches
+    -geno-id, -gid   : genotype IDs are in the first row, if phenotype IDs are also present then we check for mismatches
+    -pheno-col, -pc  : column for phenotype values, default is 1 if phenotype IDs are not present, 2 otherwise
+    -geno-skip, -gs  : column at which genotype values start, preceding columns are printed
+    -perm            : calculated permuted p values, one following number indicates the number of permutations, two comma separated numbers gives the number of permutations and the seed
+    -pval            : report permutation p values for each test (needs perm options to be specified)
+    -fwer            : calculates Family Wise Error Rate based on permutations
+    -nocheck         : skip check of IDs when both genotype and phenotype IDs are present
+
+Input file formats:
+    phenotype        : Tab or whitespace separated file with phenotype values in column specified by -pc, and optional subject IDs in column 1
+    genotype         : Tab or whitespace separated file where each row corresponds to single SNP, optional header line can contain subject IDs, number of columns specified by -gs are copied to results file
+
+Output:
+    Output contains the first info columns from the genotype file, followed by spearman correlation, t statistic, p value columns. When permutations are analysed, the p value calculated by permutations is printed if the -pval flag is used. The p value calculated by permutations and then the p value adjusted for multiple testing is shown if -fwer is used. If neither flag is present, then p values for calculated on permuted datasets are reported next.
+";
+
+void giveHelp(){
+  writeln(helpString);
+  exit(0);
+}
+
+ 
+string[string] getOpts(in string[] args){
+  string[string] opts;
+  string prefix;
+  immutable string[string] optsDictParam = ["p" : "p", "phenotype" : "p", "g" : "g", "genotype" : "g", 
+					    "cov" : "cov", "pc" : "pc", "pheno-col" : "pc", "gs" : "gs",
+					    "geno-skip" :"gs", "perm" :"perm", "output" : "o", "o" : "o"];
+
+  immutable string[string] optsDictFlag = ["gid" : "gid", "geno-id" : "gid", "pid" : "pid", "nocheck" : "nocheck",
+					   "pheno-id" : "pid", "pval" : "pval", "fwer" : "fwer"];
+ 
+
+  foreach(i, arg; args)
+    {
+      if (arg.startsWith("-"))
+	{
+	  prefix = chompPrefix(arg.idup, "-");
+	  if (prefix=="-help")
+	    giveHelp();
+	  auto pParam = prefix in optsDictParam;
+	  if (pParam)
+	    {
+	      if (i==(args.length - 1))
+		{
+		  writeln("Failed to run analysis: Missing parameter for -", prefix, " option");
+		  exit(0);
+		}
+	      opts[*pParam] = args[i+1].idup;
+	    }
+	  else 
+	    {
+	      auto pFlag = prefix in optsDictFlag;
+	      if (pFlag)
+		opts[*pFlag] = "T";
+	      else
+		{
+		  writeln("Failed to run analysis: Unknown command -", prefix);
+		  exit(0);
+		}
+	    }
+	}
+    }
+
+  if (!("p" in opts))
+    opts["p"] = args[args.length - 1];
+
+  return opts;
+}
+
---
+++ missing/calculation.d
@@ -0,0 +1,135 @@
+import std.algorithm : makeIndex;
+import std.array : split;
+import std.conv : to, ConvException;
+import std.math : fabs, sqrt;
+import std.random : rndGen, randomShuffle;
+import std.range : lockstep;
+import std.stdio : File, writeln;
+
+import arg_parse : Opts;
+import run_analysis : InputException;
+
+extern(C) {
+  double gsl_cdf_tdist_P(double x, double nu);
+}
+
+extern(C) {
+  void regress(size_t nInd, size_t nCov, double *x, double *y, double *rOut);
+}
+
+class VarianceException : Exception {
+  this(string s) {super(s);}
+}
+
+void covariates(string covF, ref double[] phen){
+  double[] covOut;
+  auto covFile = File(covF);
+  size_t nInd = 1;
+  covOut ~= 1;
+
+  auto firstLine = split(covFile.readln());
+  size_t nCov = firstLine.length;
+  covOut ~= to!(double[])(firstLine);
+
+  foreach(line; covFile.byLine())
+    {
+      covOut ~= 1;
+      auto splitLine = split(line);
+	{
+	  if(splitLine.length != nCov)
+	    throw new InputException("Failed to run analysis, covariate measurements missing");
+	  else
+	    {
+	      covOut ~= to!(double[])(splitLine);
+	      nInd++;
+	    }
+	}
+    }
+
+  if (nInd != phen.length)
+    throw new InputException("Failed to run analysis, covariates file does not have the same number of individuals as phenotype file");
+
+  regress(nInd, nCov + 1, covOut.ptr, phen.dup.ptr, phen.ptr);
+}
+
+
+ref double[] rank(ref double[] rankArray){
+
+  immutable size_t len = rankArray.length;
+  auto orderIndex = new size_t[len];
+  makeIndex!("a < b")(rankArray, orderIndex);
+
+  double sumrank = 0.0;
+  size_t dupcount = 0;
+  double avgrank;
+
+  foreach(i, ref e; orderIndex)
+    {
+      sumrank += i;
+      dupcount++;
+      if (i == (len - 1) || rankArray[e] != rankArray[orderIndex[i + 1]])
+	{
+	  avgrank = sumrank / dupcount + 1;
+	  for(auto j = i - dupcount + 1; j < i + 1; j++)
+	    rankArray[orderIndex[j]] = avgrank;
+	  sumrank = 0;
+	  dupcount = 0;
+	}
+    }
+  return rankArray;
+}
+
+void transform(ref double[] vector){
+  int n = 0;
+  double mean = 0;
+  double M2 = 0;
+  double delta;
+
+  foreach(ref e; vector)
+    {
+      n++;
+      delta = e - mean;
+      mean += delta / n;
+      M2 += delta * (e - mean);
+    }
+
+  if (M2==0)
+    throw new VarianceException("");
+
+  M2 = sqrt(M2);
+
+  foreach(ref e; vector)
+    e = (e - mean) / M2;
+
+}
+
+double[3] correlation(in double[] vector1, immutable(double[]) vector2){
+  double[3] results = 0;
+
+  foreach(ref a, ref b; lockstep(vector1, vector2))
+    results[0] += a * b;
+
+  results[1] = results[0] * sqrt((vector1.length - 2) / (1 - results[0] * results[0]));
+  results[2] = gsl_cdf_tdist_P(-fabs(results[1]), vector1.length - 2) * 2;
+  return results;
+}
+
+ref double corPvalue(ref double results, in size_t len){
+  results = results * sqrt((len - 2) / (1 - results * results));
+  results = gsl_cdf_tdist_P(-fabs(results), len - 2) * 2;
+  return results;
+}
+
+double[] getPerm(in Opts permOpts, immutable(double[]) vector){
+  if (permOpts.give_seed)
+    rndGen.seed(permOpts.seed);
+
+  double[] outPerm = new double[vector.length * permOpts.number];
+  for(auto i = 0; i < permOpts.number; i++)
+    {
+      outPerm[(i * vector.length)..(i + 1) * vector.length] = vector.dup;
+      randomShuffle(outPerm[(i * vector.length)..(i + 1) * vector.length]);
+    }
+
+  return outPerm;
+}
